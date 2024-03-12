9a03dd9aeb9151e46edaf411dea85c596b2e7f0e
Nhan <nhanptde160251@fpt.edu.vn>
2024-03-12 13:37:44 +0900

    UploadFile still error

--- Mock_Project_Group9/Pages/Products/Create.cshtml.cs
+++ Mock_Project_Group9/Pages/Products/Create.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -13,12 +14,18 @@ namespace Mock_Project_Group9.Pages.Products
     public class CreateModel : PageModel
     {
         private readonly Mock_Project_Group9.Database.WebDBContext _context;
+        private IHostEnvironment _enviroment;
 
-        public CreateModel(Mock_Project_Group9.Database.WebDBContext context)
+
+        public CreateModel(Mock_Project_Group9.Database.WebDBContext context, IHostEnvironment enviroment)
         {
             _context = context;
+            _enviroment = enviroment;
         }
 
+        [DataType(DataType.Upload)]
+        [BindProperty]
+        public IFormFile? FileUpload { get; set; }
         public IActionResult OnGet()
         {
         ViewData["CategoryId"] = new SelectList(_context.categories, "CategoryId", "CategoryName");
@@ -33,6 +40,7 @@ namespace Mock_Project_Group9.Pages.Products
         public async Task<IActionResult> OnPostAsync()
         {
             Product.ProductId = Guid.NewGuid();
+            Task image = GetImage();
           if (!ModelState.IsValid || _context.products == null || Product == null)
             {
                 return Page();
@@ -42,5 +50,25 @@ namespace Mock_Project_Group9.Pages.Products
 
             return RedirectToPage("./Index");
         }
+
+        public async Task GetImage()
+        {
+            {
+                if (FileUpload != null)
+                {
+                        var file = Path.Combine(_enviroment.ContentRootPath, "images", FileUpload.FileName);
+                        using (var fileStream = new FileStream(file, FileMode.Create))
+                        {
+                            await FileUpload.CopyToAsync(fileStream);
+                        }
+                        Product.Images = file.ToString();
+                    }
+                else
+                {
+                    Product.Images = "images/No_Image_Available.jpg";
+                }
+            }
+            }
+
+        }
     }
-}
