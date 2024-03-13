4b4ac0921c8a402cb1dff80e6043a218182d2ee3
Nhon Tran <nhonvn2509@gmail.com>
2024-03-13 20:34:00 +0700

    fix UI and crud

---
+++ Mock_Project_Group9/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mock_Project_Group9.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Mock_Project_Group9/Pages/Product.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Mock_Project_Group9.Database;
+using Mock_Project_Group9.Models.Products;
+
+namespace Mock_Project_Group9.Pages
+{
+    public class ProductModel : PageModel
+    {
+        private readonly Mock_Project_Group9.Database.WebDBContext _context;
+
+        public ProductModel(Mock_Project_Group9.Database.WebDBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.products != null)
+            {
+                Product = await _context.products
+                .Include(p => p.Category).ToListAsync();
+            }
+        }
+    }
+}
